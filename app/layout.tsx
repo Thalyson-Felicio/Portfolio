89c05af4623b3232bc5f88cf746cb358ab65dc13
Thalyson Felicio <thalyson.felicio@academico.ifpb.edu.br>
2025-11-14 18:11:49 -0300

    Fiz as primeiras paginas do portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+
+const font= Inter ({
+  weight:['100','200','300','400','500','600','700','800','900'],
+  subsets:["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Thalyson Portfolio | next.js 15",
+  description: "Portfolio build with Next.js 15",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
+        <ResponsiveNav/>
+        {children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Home from '@/components/Home/Home';
+import React from 'react'
+
+const HomePage = () => {
+  return <Home />;
+};
+
+export default HomePage;
+
+// LocaLhost:3000
+// LocaLhost:3000/about
+// LocaLhost:3000/about/team
+// LocaLhost:3000/about/team/Thalyson
