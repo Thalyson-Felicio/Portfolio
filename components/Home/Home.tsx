c52b368be5da293dacd0abe804f531be85b4dc3d
thalysonFelicio <thalysonbr1999@gmail.com>
2025-11-26 16:04:19 -0300

    Fiz a primeira parte do my work experience

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Hero from './Hero/Hero';
+import Services from './Services/Services';
+import Resume from './Resume/Resume';
 
 const Home = () => {
     return <div className="overflow-hidden">
         <Hero />
+        <Services />
+        <Resume />
     </div>;
 };;
 
